45d01492a3e2ca59d28f1222f39b762dc7c6e227
gabriegsti <gabrielgsdevbr@gmail.com>
2022-05-12 20:51:39 -0300

    video 6

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-list
+      flat
+      subheader
+    >
+
+      <v-list-item-group
+        v-model="settings"
+        multiple
+        active-class=""
+      >
+        
+      <Tarefa/>
+      <Tarefa/>
+      <Tarefa/>
+        
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import Tarefa from '../components/tarefas/tarefas.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Tarefa      
+    },
+  }
+</script>
